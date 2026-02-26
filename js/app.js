48f90102a25424a4e50bb4977939e28cdf6c1a19
ideav <56253892+ideav@users.noreply.github.com>
2026-02-26 16:34:20 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -355,7 +355,7 @@ const CookieUtil = {
 class ApiConfig {
     constructor() {
         this.host = localStorage.getItem('apiHost') || window.location.hostname;
-        this.yandexClientId = localStorage.getItem('yandexClientId') || '';
+        this.yandexClientId = '959da92b09364e42bf4c7704db0b992f';
     }
 
     getBaseUrl(db) {
